a9de98357baf1ec9333af973361579c960913420
joseortiz871 <joseluisangulo124@gmail.com>
2025-12-10 11:29:04 -0500

    practica javascript factura

--- js/Funcionesfacturas.js
+++ js/Funcionesfacturas.js
@@ -4,12 +4,29 @@ var numProductos = 0;
 function addfila() {
     var tabla = document.getElementById("tblfactura").getElementsByTagName('tbody')[0];
     var fila = tabla.insertRow(-1);
-    console.log(tabla);
-    var celda1 = fila.insertCell(0) ;
+    //console.log(tabla);
+    var celda1 = fila.insertCell(0);
     var celda2 = fila.insertCell(1);
     var celda3 = fila.insertCell(2);
     var celda4 = fila.insertCell(3);
     var celda5 = fila.insertCell(4);
     var celda6 = fila.insertCell(5);
-    celda1.innerHTML = '<input width ="211px" type="text" id="producto' + numProductos + '" name="producto' + numProductos + '" class="form-control" />';
-}
+
+    var botonEliminar = document.createElement("button");
+    botonEliminar.innerHTML = "-";
+    botonEliminar.onclick = function () {
+        var row = this.parentNode.parentNode;
+        row.remove();
+        numProductos--;
+        var contador = document.getElementById("numProductos");
+        contador.innerHTML = numProductos = "<b>" + numProductos + "</b>";
+    }
+    celda1.appendChild(botonEliminar);
+
+    numProductos++;
+
+
+    var contador = document.getElementById("numProductos");
+    contador.innerHTML ="<b>" + numProductos + "</b>";
+
+}   
